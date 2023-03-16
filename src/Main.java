3b65c4eb4eefccf2f92e2be723762fe628e9eb54
Aniket Kumar <ani6393knp@gmail.com>
2023-03-16 11:16:25 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+         String str = "manish kumar panday";
+
+        Map<Character, Long> collect = str.chars().mapToObj(c -> (char) c).filter(Character::isLetter)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+
+
+
+
+    }
+}
